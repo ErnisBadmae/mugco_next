082a871bde79d6c6c1e0ad7f13f26c82777e5a69
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-06-13 13:31:29 +0300

    fix:store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,43 +1,50 @@
-import { cartSlice } from './slices/cartSlice';
-import { userSlice } from './user/user.slice';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import {
-  FLUSH,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-  REHYDRATE,
-  persistReducer,
-  persistStore,
-} from 'redux-persist';
-// import persistReducer from 'redux-persist/es/persistReducer';
-// import persistStore from 'redux-persist/es/persistStore';
-import storage from 'redux-persist/lib/storage';
+	FLUSH,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+	REHYDRATE,
+	persistReducer,
+	persistStore
+} from 'redux-persist'
 
-const persistConfig = {
-  key: 'mugco',
-  storage,
-  whitelist: ['cart'],
-};
+import { cartSlice } from './slices/cartSlice'
+import { userSlice } from './user/user.slice'
 
-const rootReducer = combineReducers({
-  cart: cartSlice.reducer,
-  // carousel: carouselSlice.reducer,
-  user: userSlice.reducer,
-});
+const isClient = typeof window !== 'undefined'
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const combinedReducers = combineReducers({
+	cart: cartSlice.reducer,
+	// carousel: carouselSlice.reducer,
+	user: userSlice.reducer
+})
+
+let mainReducer = combinedReducers
+
+if (isClient) {
+	const { persistReducer } = require('redux-persist')
+	const storage = require('redux-persist/lib/storage').default
+
+	const persistConfig = {
+		key: 'mugco',
+		storage,
+		whitelist: ['cart']
+	}
+
+	mainReducer = persistReducer(persistConfig, combinedReducers)
+}
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-});
+	reducer: mainReducer,
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+			}
+		})
+})
 
-export const persistor = persistStore(store);
-export type TypeRootState = ReturnType<typeof rootReducer>;
+export const persistor = persistStore(store)
+export type TypeRootState = ReturnType<typeof mainReducer>
