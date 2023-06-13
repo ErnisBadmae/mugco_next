3e6764d02415cb843c9f94cf661810d1f2945e34
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-06-13 13:24:27 +0300

    fix: auth provider

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+
+import Heading from '../components/ui/heading/Heading'
+import Layout from '../components/ui/layout/Layout'
+
+export default function NotFound() {
+	return (
+		<Layout>
+			<Heading>Not found</Heading>
+			<p>Could not find requested resource</p>
+			<p>
+				View{''}
+				<Link href='/explorer' className='text-primary'>
+					all products
+				</Link>
+			</p>
+		</Layout>
+	)
+}
--- src/providers/auth-provider/AuthProvider.tsx
+++ src/providers/auth-provider/AuthProvider.tsx
@@ -1,22 +1,27 @@
 'use client'
 
 import Cookies from 'js-cookie'
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import { FC, PropsWithChildren, useEffect } from 'react'
 
+import NotFound from '../../app/not-found'
+import { ADMIN_PANEL_URL } from '../../config/url.config'
 import { REFRESH_TOKEN } from '../../constants/token.constants'
 import { useActions } from '../../hooks/useActions'
 import { useAuth } from '../../hooks/useAuth'
 import { getAccessToken } from '../../services/auth/auth.helper'
 
+import { protectedRoutes } from './protected-routes.data'
+
 const AuthProvider: FC<PropsWithChildren<unknown>> = ({ children }) => {
-	const { user } = useAuth()
-	const { checkAuth, logout } = useActions()
 	const pathname = usePathname()
+	const router = useRouter()
+
+	const { checkAuth, logout } = useActions()
+	const { user } = useAuth()
 
 	useEffect(() => {
 		const accessToken = getAccessToken()
-
 		if (accessToken) checkAuth()
 	}, [])
 
@@ -25,11 +30,39 @@ const AuthProvider: FC<PropsWithChildren<unknown>> = ({ children }) => {
 		if (!refreshToken && user) logout()
 	}, [pathname])
 
-	return isOnlyUser ? (
-		<DynamicCheckRole Component={{ isOnlyUser }}>{children}</DynamicCheckRole>
-	) : (
-		<>{children}</>
+	const isProtectedRoute = protectedRoutes.some(route =>
+		pathname?.startsWith(route)
 	)
+
+	const isAdminRoute = pathname?.startsWith(ADMIN_PANEL_URL)
+
+	//общий для всех
+
+	const getUserStatus = (): string => {
+		if (!isProtectedRoute && !isAdminRoute) return 'GENERAL'
+		if (user?.isAdmin) return 'ADMIN'
+		if (user && isProtectedRoute) return 'PROTECTED'
+		if (user && isAdminRoute) return 'NOT_FOUND'
+		if (pathname !== '/auth') return 'AUTH'
+		return 'UNKNOWN'
+	}
+
+	switch (getUserStatus()) {
+		case 'GENERAL':
+		case 'ADMIN':
+		case 'PROTECTED':
+			return <>{children}</>
+		case 'NOT_FOUND':
+			return (
+				<>
+					<NotFound />
+				</>
+			)
+		case 'AUTH':
+			router.replace('/auth')
+		default:
+			return null
+	}
 }
 
 export default AuthProvider
